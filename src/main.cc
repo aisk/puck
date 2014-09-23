db64738883ae7560a491b24bf9c3bc2e35e8cf0a
asaka <aisk1988@gmail.com>
2014-09-23 17:55:53 +0800

    modify main function

--- src/main.cc
+++ src/main.cc
@@ -4,34 +4,52 @@
 #include "parser.h"
 #include "eval.h"
 #include "object.h"
+#include "state.h"
+
+using namespace rv;
+
+int main() {
+    puts("Welcome to rivai.");
+    char buffer[256];
+    InitGlobalState();
+    while (true) {
+        printf(">> ");
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+            break;
+        }
+        puts(buffer);
+    }
+
+    DestroyGlobalState();
+}
 
 rv_obj* rv_cfunc_add(rv_obj* params) {
     // puts("hehe");
     return rv_integer_new(123);
 }
 
-int main() {
-    char buffer[256];
-    rv_env* global_env = rv_env_new(NULL);
-    rv_env_set(global_env, rv_string_new(const_cast<char *>("+")), rv_cfunc_new(const_cast<char *>("+"), rv_cfunc_add));
-    while (true) {
-        printf(">> ");
-        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
-            break;
-        }
-        rv_parser* parser = rv_parser_new(buffer);
-        // printf("[PARSE] ");
-        rv_obj* obj = rv_parse(parser);
-        // rv_obj_print(obj);
-        // printf("\n");
-        free(parser);
-        rv_obj* ret_obj = rv_eval(obj, global_env);
-        if (ret_obj != NULL) {
-            printf("=> ");
-            rv_obj_print(ret_obj);
-            printf("\n");
-        }
-    }
-
-    return 0;
-}
+// int main() {
+//     char buffer[256];
+//     rv_env* global_env = rv_env_new(NULL);
+//     rv_env_set(global_env, rv_string_new(const_cast<char *>("+")), rv_cfunc_new(const_cast<char *>("+"), rv_cfunc_add));
+//     while (true) {
+//         printf(">> ");
+//         if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+//             break;
+//         }
+//         rv_parser* parser = rv_parser_new(buffer);
+//         // printf("[PARSE] ");
+//         rv_obj* obj = rv_parse(parser);
+//         // rv_obj_print(obj);
+//         // printf("\n");
+//         free(parser);
+//         rv_obj* ret_obj = rv_eval(obj, global_env);
+//         if (ret_obj != NULL) {
+//             printf("=> ");
+//             rv_obj_print(ret_obj);
+//             printf("\n");
+//         }
+//     }
+// 
+//     return 0;
+// }
